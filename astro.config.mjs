7872090116c3b5d179425f7089a2699b2c0aaf54
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2024-10-10 01:56:30 +0200

    confirmar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: "server",
   adapter: vercel(),
 });
